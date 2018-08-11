b79df70a03f9d9e1b925007c3853989dffeeb90b
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2018-08-11 21:15:02 +0000

    read excel write json

--- src/upload_to_s3.py
+++ src/upload_to_s3.py
@@ -1,11 +1,26 @@
+import pandas as pd
+
 def lambda_handler(event, context):
-    # TODO implement
+    # print values to see if lambda_handler triggered
     print (event['key1'])
     print (event['key2'])
     print (event['key3'])
-    return 'Hello from Lambda'
+
+    print("reading excel")
+    data_frame = pd.read_excel('C:\steel-eye\ISO10383_MIC.xls', sheetname='MICs Modifications')
+
+    row_list = data_frame.to_dict(orient='row')
+
+    print('num of rows: %d ' % len(row_list))
+
+    with open('C:\steel-eye\data.json', 'wb') as outfile:
+        json.dump(row_list, outfile)
+
+    return 'Returning from Lambda'
 
 if __name__ == "__main__":
+
+    # simulate the event and context object for testing locally out of Lambda
     class Event:
         def __init__(self):
             self.key1 = 'val1'
