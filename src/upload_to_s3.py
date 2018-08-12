9033fab9b56da7033e3a684ee6a3bd1424b31c10
ChaitanyaVardhan <chaitanyavardhan1@gmail.com>
2018-08-12 00:05:58 +0000

    read excel from S3 upload json to S3

--- src/upload_to_s3.py
+++ src/upload_to_s3.py
@@ -1,20 +1,35 @@
 import pandas as pd
 
+import boto3
+
+import json
+
+import os
+
 def lambda_handler(event, context):
     # print values to see if lambda_handler triggered
-    print (event['key1'])
-    print (event['key2'])
-    print (event['key3'])
+    print (str(event))
+
+    s3_client = boto3.client('s3',
+                aws_access_key_id=os.environ.get('aws_access_key_id'),
+                aws_secret_access_key=os.environ.get('aws_secret_access_key'))
 
     print("reading excel")
-    data_frame = pd.read_excel('C:\steel-eye\ISO10383_MIC.xls', sheetname='MICs Modifications')
+    data_frame = pd.read_excel('https://s3.amazonaws.com/json-file-bucket/ISO10383_MIC.xls'
+, sheet_name='MICs Modifications', engine=None)
 
     row_list = data_frame.to_dict(orient='row')
 
     print('num of rows: %d ' % len(row_list))
 
-    with open('C:\steel-eye\data.json', 'wb') as outfile:
+    with open('data.json', 'w') as outfile:
         json.dump(row_list, outfile)
+
+    bucket_name = 'json-file-bucket'
+
+    file_name = 'data.json'
+
+    file_object = s3_client.put_object(Bucket=bucket_name, Key=file_name)    
 
     return 'Returning from Lambda'
 
